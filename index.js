184599909098cbba6d4628e84e1d1109f2b6ee67
Ben Robbins <ben.robbins@gmail.com>
2015-05-23 13:37:50 -0500

    initial implementation

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var ffi = require('ffi');
 
 var murmur = ffi.Library('lib/target/release/libmurmur.so', {
-  murmur: ['int', ['string']]
+  murmur: ['int', ['string', 'int']]
 });
 
-module.exports = murmur.murmur;
+module.exports = function(input, seed) {
+  input = Buffer.isBuffer(input) ? input.toString('hex') : input;
+  return murmur.murmur(input, seed || 0x1234567);
+};
