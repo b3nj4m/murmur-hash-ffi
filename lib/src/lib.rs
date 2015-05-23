17516333d8d2459ddf7e5b0eb73cae9287e27a6c
Ben Robbins <ben.robbins@gmail.com>
2015-05-23 13:48:22 -0500

    remove some cruft

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -23,13 +23,13 @@ pub extern fn murmur(input: &str, seed: u32) -> u32 {
       k1 = mem::transmute::<[u8; 4], u32>([chunk[0], chunk[1], chunk[2], chunk[3]]);
     }
 
-    k1 = ((((k1 & 0xffff) * c1) + ((((k1 >> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+    k1 = ((k1 & 0xffff) * c1) + ((((k1 >> 16) * c1) & 0xffff) << 16);
     k1 = (k1 << 15) | (k1 >> 17);
-    k1 = ((((k1 & 0xffff) * c2) + ((((k1 >> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+    k1 = ((k1 & 0xffff) * c2) + ((((k1 >> 16) * c2) & 0xffff) << 16);
 
     h1 ^= k1;
     h1 = (h1 << 13) | (h1 >> 19);
-    h1b = ((((h1 & 0xffff) * 5) + ((((h1 >> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+    h1b = ((h1 & 0xffff) * 5) + ((((h1 >> 16) * 5) & 0xffff) << 16);
     h1 = ((h1b & 0xffff) + 0x6b64) + ((((h1b >> 16) + 0xe654) & 0xffff) << 16);
   }
 
@@ -47,17 +47,17 @@ pub extern fn murmur(input: &str, seed: u32) -> u32 {
     remainder -= 1;
   }
 
-  k1 = (((k1 & 0xffff) * c1) + ((((k1 >> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+  k1 = ((k1 & 0xffff) * c1) + ((((k1 >> 16) * c1) & 0xffff) << 16);
   k1 = (k1 << 15) | (k1 >> 17);
-  k1 = (((k1 & 0xffff) * c2) + ((((k1 >> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+  k1 = ((k1 & 0xffff) * c2) + ((((k1 >> 16) * c2) & 0xffff) << 16);
   h1 ^= k1;
 
   h1 ^= inputBytes.len() as u32;
 
   h1 ^= h1 >> 16;
-  h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+  h1 = ((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >> 16) * 0x85ebca6b) & 0xffff) << 16);
   h1 ^= h1 >> 13;
-  h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+  h1 = ((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >> 16) * 0xc2b2ae35) & 0xffff) << 16);
   h1 ^= h1 >> 16;
 
   return h1 >> 0 as u32;
